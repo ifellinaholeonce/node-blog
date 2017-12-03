f3aa6a8baf5542aab30307e75632f8656fad3379
Tymm Schmitke <ifellinaholeonce@gmail.com>
2017-12-03 22:35:39 +0000

    tutorial finished. no problems. home reads hardcoded posts. node and express handle the requests

---
+++ app.js
@@ -0,0 +1,58 @@
+/* app.js */
+
+var app = require('express')();
+
+var posts = [
+  {
+    id:1,
+    author: 'John',
+    title: 'Templating with EJS',
+    body: 'Blog post number 1'
+  },
+  {
+    id:2,
+    author: 'Drake',
+    title: 'Express',
+    body: 'Blog post number 2'
+  },
+  {
+    id:3,
+    author: 'Emma',
+    title: 'Streams',
+    body: 'Blog post number 1'
+  },
+  {
+    id:4,
+    author: 'Keira',
+    title: 'Events',
+    body: 'Blog post number 1'
+  },
+];
+
+//set view engine to ejs
+app.set('view engine', 'ejs');
+
+//blog home
+app.get('/', function(req, res) {
+  //render home.ejs
+  res.render('home', { posts: posts });
+});
+
+//blog post
+app.get('/post/:id', function(req, res) {
+  //find the post in the posts array
+  var post = post.filter((post) => {
+    return post.id == req.params.id;
+  })[0];
+
+  //render the post.ejs template
+  res.render('post', {
+    author: post.author,
+    title: post.title,
+    body: post.body
+  });
+});
+
+app.listen(8080);
+
+console.log('listening on port 8080');
